6ebb311b0e4b64c33f015c089f5c71caf7a4c884
MD. SHAHIDUL ISLAM <shahidul7889@gmail.com>
2018-08-25 11:37:36 +0600

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+var krms_config ={			
+	'ApiUrl':" http://55e599b3.ngrok.io/restomulti/mobileapp/api",		
+	'DialogDefaultTitle':"KMRS",	
+	'pushNotificationSenderid':"691459402343",	
+	'facebookAppId':"1403179106460986",
+	'APIHasKey':"6258b8b179f5f1a1925f412cf74992a"
+};
+
